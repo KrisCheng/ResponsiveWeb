6b18fafc34cb3cff4090f86c02198cec0f3846a9
Kris_Chan <743628145@qq.com>
2017-03-07 19:10:46 +0800

    1. the responsive style;

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,12 @@
  * Created by KrisChan on 02/03/2017.
  * All right reserved.
  */
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 1000,
+        autoplayHoverPause:true
+    });
+});
